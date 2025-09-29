ffc9bdb3a9e6e9c79dc724ad32b1eb2ce5e3cab7
krak <notreallykrak@gmail.com>
2025-09-29 19:21:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
 module.exports = {
     // Discord hesabınızın token'ı
-    token: 'Token',
+    token: 'Tokenini Buraya Gir',
     
+    // Kamera modunda oynatılacak sabit video veya GIF linki
+    cameraVideoUrl: 'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExajRwMnNyajZrb3ZncnZkcXdsNzk4cjVnbm5pMnp6cmN5cG5mb2VzbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/y0NFayaBeiWEU/giphy.gif',
+
     // AFK mesajı (Değiştirebilirsiniz)
-    afkMessage: 'Şu anda AFK yım!',
-    
-    // Bot ayarları (Bu ayarları değiştirmeyin)
-    settings: {
-        // Mesajlar arası bekleme süresi (ms cinsinden) - Discord spam koruması için
-        messageDelay: 10000,
-        
-        // Aynı kişiye tekrar mesaj gönderme süresi (ms cinsinden)
-        cooldownTime: 30000
-    }
+    afkMessage: 'Şu anda meşgulüm, daha sonra döneceğim.',
 };
